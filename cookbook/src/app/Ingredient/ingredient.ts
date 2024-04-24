02d4c966c6f0915c37fd4092d40b0295629047bd
naaaani <nandi.zalka@gmail.com>
2024-04-24 10:30:04 +0200

    Add ingredient.ts interface

---
+++ cookbook/src/app/Ingredient/ingredient.ts
@@ -0,0 +1,9 @@
+export interface Ingredient {
+    id?: number;
+    name?: string;
+    unitOfMeasure?: string;
+    isGlutenFree?: boolean;
+    isDairyFree?: boolean;
+    isMeatFree?: boolean;
+    isEggFree?: boolean;
+}
