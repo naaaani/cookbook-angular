5e1e82709e40b2106be74f5ef4bf7246e784c7f0
naaaani <nandi.zalka@gmail.com>
2024-04-25 11:40:10 +0200

    Add ingredients route

--- cookbook/src/app/app.component.ts
+++ cookbook/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
