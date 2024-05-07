0984799f1b79d1d90e71bfbb5eb8ccf349f26d74
naaaani <nandi.zalka@gmail.com>
2024-05-07 10:21:05 +0200

    Add recipe-card component

---
+++ cookbook/src/app/Recipe/recipe-card/recipe-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Recipe } from '../recipe';
+
+@Component({
+  selector: 'app-recipe-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe-card.component.html',
+  styleUrl: './recipe-card.component.css'
+})
+
+export class RecipeCardComponent {
+  @Input() recipe: Recipe;
+}
