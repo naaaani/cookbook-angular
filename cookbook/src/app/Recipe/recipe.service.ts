6a85fefdefa983b35cd45d89cafba567354255b3
Nikolett Hevizi <nikihevizi@gmail.com>
2024-05-07 10:10:47 +0200

    some more

---
+++ cookbook/src/app/Recipe/recipe.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  url = "/api/recipes";
+
+  async getAllIRecipes(): Promise<Recipe[]> {
+
+    const data = await fetch(this.url);
+    console.log(data);
+    
+    return data.json() ?? [];
+  }
+
+  async getRecipe(id : number): Promise<Recipe[]> {
+        
+    const data = await fetch(this.url + '/' + (id));
+    console.log(data);
+    
+    return data.json() ?? [];
+  }
+
+  constructor() { }
+}
--- cookbook/src/app/app.routes.ts
+++ cookbook/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { IngredientListComponent } from './Ingredient/ingredient-list/ingredient-list.component';
 import { RegistrationFormComponent } from './Authentication/registration-form/registration-form.component';
 import { LoginFormComponent } from './Authentication/login-form/login-form.component';
+import { RecipeDetailsComponent } from './Recipe/recipe-details/recipe-details.component';
 
 
 export const routes: Routes = [
     { path: "ingredients", component: IngredientListComponent },
     { path: "registration", component: RegistrationFormComponent },
     { path: "login", component: LoginFormComponent },
+    { path: "recipes", component: RecipeDetailsComponent },
 ];
