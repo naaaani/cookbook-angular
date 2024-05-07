acc878dadeb255f0ca486edc5b66d035f1fda0c6
naaaani <nandi.zalka@gmail.com>
2024-05-07 15:01:55 +0200

    Add Icons, MatIconModule

--- cookbook/src/app/Recipe/recipe-card/recipe-card.component.ts
+++ cookbook/src/app/Recipe/recipe-card/recipe-card.component.ts
@@ -1,10 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../recipe';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-recipe-card',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatIconModule
+  ],
   templateUrl: './recipe-card.component.html',
   styleUrl: './recipe-card.component.css'
 })
--- cookbook/src/app/app.component.ts
+++ cookbook/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -14,4 +16,26 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'cookbook';
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    matIconRegistry.addSvgIcon(
+      `gluten_free`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl(`../../assets/icons/gluten-free-svgrepo-com.svg`)
+    )
+    matIconRegistry.addSvgIcon(
+      `vegan`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl(`../../assets/icons/vegan-svgrepo-com.svg`)
+    )
+    matIconRegistry.addSvgIcon(
+      `dairy_free`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl(`../../assets/icons/dairy-free-svgrepo-com.svg`)
+    )
+    matIconRegistry.addSvgIcon(
+      `vegetarian`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl(`../../assets/icons/food-no-meat-svgrepo-com.svg`)
+    )
+  }
 }
