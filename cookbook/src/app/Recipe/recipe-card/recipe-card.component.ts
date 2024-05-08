5b2127a3771df2874d322561c0c9e2cc4b36702a
naaaani <nandi.zalka@gmail.com>
2024-05-08 14:03:42 +0200

    Make icons appear conditionally

--- cookbook/src/app/Recipe/recipe-card/recipe-card.component.ts
+++ cookbook/src/app/Recipe/recipe-card/recipe-card.component.ts
@@ -1,14 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../recipe';
-import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-card',
   standalone: true,
   imports: [
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    NgIf
   ],
   templateUrl: './recipe-card.component.html',
   styleUrl: './recipe-card.component.css'
