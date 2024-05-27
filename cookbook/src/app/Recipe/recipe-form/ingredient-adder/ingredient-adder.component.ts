9085f5963c03d82766ac18f918a1c055fdd1c0b9
naaaani <nandi.zalka@gmail.com>
2024-05-27 17:28:12 +0200

    Add starting design

--- cookbook/src/app/Recipe/recipe-form/ingredient-adder/ingredient-adder.component.ts
+++ cookbook/src/app/Recipe/recipe-form/ingredient-adder/ingredient-adder.component.ts
@@ -1,10 +1,26 @@
-import { Component} from '@angular/core';
+import { Component, EventEmitter, Output} from '@angular/core';
 import { NgFor } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatIcon } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { Ingredient } from '../../../Ingredient/ingredient';
 import { IngredientService } from '../../../Ingredient/ingredient.service';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+
+interface IngredientForRecipe {
+  id: null
+      ingredient?: {
+        id: number,
+        name: "string",
+        unitOfMeasure: "string",
+        isGlutenFree: boolean,
+        isDairyFree: boolean,
+        isMeatFree: boolean,
+        isEggFree:boolean
+      },
+      amount: number
+}
 
 @Component({
   selector: 'app-ingredient-adder',
@@ -13,24 +29,49 @@ import { IngredientService } from '../../../Ingredient/ingredient.service';
     NgFor,
     MatInputModule,
     MatIcon,
-    MatCardModule
+    MatCardModule,
+    MatAutocompleteModule,
   ],
   templateUrl: './ingredient-adder.component.html',
   styleUrl: './ingredient-adder.component.css'
 })
+
 export class IngredientAdderComponent {
-  ingredients: Ingredient[] = [];
-  
-  bumpIngredients(): void {
-    this.ingredients.push({})
+  savedIngredients: Ingredient[] = [];
+  ingredientsForm: FormGroup;
+  @Output() ingredientsAdded = new EventEmitter<{ amount: string, ingredient: string }[]>();
+
+  constructor(private fb: FormBuilder, private service: IngredientService) {
+    this.ingredientsForm = this.fb.group({
+      ingredients: this.fb.array([this.createIngredient()])
+    });
+
+    this.service.getAllIngredients()
+    .then(data => this.savedIngredients = data);
+
   }
 
-  deleteIngredient(id: number): void {
-    this.ingredients.splice(id, 1);
+  get ingredients() {
+    return this.ingredientsForm.get('ingredients') as FormArray;
   }
 
-  constructor(private service: IngredientService) {
-    this.ingredients.push({}, {})
+  createIngredient(): FormGroup {
+    return this.fb.group({
+      amount: '',
+      ingredient: ''
+    });
+  }
+
+  addIngredient() {
+    this.ingredients.push(this.createIngredient());
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.removeAt(index);
+  }
+
+  submitIngredients() {
+    this.ingredientsAdded.emit(this.ingredients.value);
   }
 
 }
