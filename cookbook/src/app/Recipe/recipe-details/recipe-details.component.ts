e8b0e8bee362c42c27610407ca2d22769ebbac4c
naaaani <nandi.zalka@gmail.com>
2024-06-04 13:20:30 +0200

    Show image on recipe details page

--- cookbook/src/app/Recipe/recipe-details/recipe-details.component.ts
+++ cookbook/src/app/Recipe/recipe-details/recipe-details.component.ts
@@ -23,10 +23,13 @@ import { Router } from '@angular/router';
     MatTooltip
   ],
 })
+
 export class RecipeDetailsComponent {
-  recipe?: Recipe;
+  linkRoot: string = 'http://localhost:4200';
   placeholder = '../../../assets/palceholder.svg';
-  imageSource = this.placeholder;
+  imageSource: string = this.placeholder;
+
+  recipe?: Recipe
 
   constructor(
     private router: Router,
@@ -39,19 +42,25 @@ export class RecipeDetailsComponent {
         this.recipeService.getRecipe(id)
           .then((recipe: Recipe) => {
             this.recipe = recipe;
+            fetch(this.linkRoot + this.recipe.imageUrl).then(res => {
+              if (res.ok) {
+                return this.recipe ? this.recipe.imageUrl : this.placeholder;
+              }
+              return this.placeholder;
+            }).then(res => this.imageSource = res);
           });
       }
     });
   }
 
-  isLoggedIn () : boolean {
-  if (localStorage.hasOwnProperty("token")) {    
-    return true;
+  isLoggedIn(): boolean {
+    if (localStorage.hasOwnProperty("token")) {
+      return true
+    }
+    return false
   }
-  return false;
-}
 
-  updateRecipe(id : any): void {
+  updateRecipe(id: any): void {
     this.router.navigate([`/update/${id}`]);
   }
 }
