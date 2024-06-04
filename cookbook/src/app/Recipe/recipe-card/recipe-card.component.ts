3b2a96760a67edc52b38842ecd0f99e59e8de2ad
naaaani <nandi.zalka@gmail.com>
2024-06-04 12:06:20 +0200

    Show image on recipe-card

--- cookbook/src/app/Recipe/recipe-card/recipe-card.component.ts
+++ cookbook/src/app/Recipe/recipe-card/recipe-card.component.ts
@@ -1,28 +1,48 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../recipe';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { NgIf } from '@angular/common';
 import { Router } from '@angular/router';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-recipe-card',
   standalone: true,
   imports: [
+    NgIf,
     MatCardModule,
     MatIconModule,
-    NgIf,
     MatTooltipModule
   ],
   templateUrl: './recipe-card.component.html',
   styleUrl: './recipe-card.component.css'
 })
 
-export class RecipeCardComponent {
-  @Input() recipe?: Recipe;
+export class RecipeCardComponent implements OnInit {
+  placeholderImg: string = '../../../assets/palceholder.svg';
+  imageSource: string = this.placeholderImg;
+  @Input() recipe: Recipe = {
+    ingredients	 : [{ingredient: {}, amount: 0}],
+    name : '',
+    description	: '',
+    isVegan	: false,
+    isVegetarian : false,
+    isGlutenFree : false,
+    isDairyFree : false,
+    imageUrl: '',
+  };
 
   constructor(private router: Router) {}
+  
+  ngOnInit(): void {
+    fetch(this.recipe.imageUrl).then(res => {
+      if (res.ok) {
+        return this.recipe.imageUrl;
+      }
+      return this.placeholderImg;
+    }).then(res => this.imageSource = res);
+  }
 
   showDetails(): void {
     this.router.navigate([`/recipe/${this.recipe?.id}`]);
